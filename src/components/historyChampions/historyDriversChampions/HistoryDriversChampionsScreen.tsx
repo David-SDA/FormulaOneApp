4fd213866a8d5bf92cfae83f0e3a58c4f7b607d0
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-03 17:01:29 +0200

    Ajout des champions des années 70

--- src/components/historyChampions/historyDriversChampions/HistoryDriversChampionsScreen.tsx
+++ src/components/historyChampions/historyDriversChampions/HistoryDriversChampionsScreen.tsx
@@ -2,12 +2,14 @@ import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import DriversChampions1950s from './DriversChampions1950s';
 import DriversChampions1960s from './DriversChampions1960s';
+import DriversChampions1970s from './DriversChampions1970s';
 
 const HistoryDriversChampions = () => {
     return (
         <ScrollView style={styles.container}>
             <DriversChampions1950s />
             <DriversChampions1960s />
+            <DriversChampions1970s />
         </ScrollView>
   )
 }
