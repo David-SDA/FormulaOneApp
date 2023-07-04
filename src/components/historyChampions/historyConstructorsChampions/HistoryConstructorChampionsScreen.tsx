ae84390cfff73a33646fdc21275e3583a35f33db
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-04 15:24:19 +0200

    Ajout de tout les constructeurs champions

--- src/components/historyChampions/historyConstructorsChampions/HistoryConstructorChampionsScreen.tsx
+++ src/components/historyChampions/historyConstructorsChampions/HistoryConstructorChampionsScreen.tsx
@@ -1,10 +1,14 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import ConstructorsChampions1958_1987 from './ConstructorsChampions1958_1987'
+import ConstructorsChampions1988_2017 from './ConstructorsChampions1988_2017'
+import ConstructorsChampions2018onwards from './ConstructorsChampions2018onwards'
 
 const HistoryConstructorChampionsScreen = () => {
   return (
     <ScrollView style={styles.container}>
+        <ConstructorsChampions2018onwards />
+        <ConstructorsChampions1988_2017 />
         <ConstructorsChampions1958_1987 />
     </ScrollView>
   )
