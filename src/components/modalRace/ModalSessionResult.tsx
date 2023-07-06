65cea98f6ed9a9e3f0de40831954b1055b899e03
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-06 15:56:11 +0200

    Ajout complet des résultats d'une course

--- src/components/modalRace/ModalSessionResult.tsx
+++ src/components/modalRace/ModalSessionResult.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Image, View, Text, StyleSheet, ActivityIndicator, ScrollView, Pressable } from 'react-native';
+import { currentConstructorColor } from '../../../constants/currentConstructorColor';
 
 const ModalSessionResult = ({round, session}) => {
     const [isLoading, setIsLoading] = useState(true);
@@ -24,7 +25,7 @@ const ModalSessionResult = ({round, session}) => {
     }, []);
 
     return (
-        <ScrollView>
+        <ScrollView showsVerticalScrollIndicator={false}>
             <View>
                 {
                     isLoading ? (
@@ -32,7 +33,7 @@ const ModalSessionResult = ({round, session}) => {
                             <ActivityIndicator size={'large'} color={'#ff1801'} />
                         </View>
                     ):(
-                        <View>
+                        <View style={{backgroundColor: '#ffffff'}}>
                             <View style={styles.titleContainer}>
                                 <Text style={styles.title}>
                                     {(session === 'results') ? 'RACE RESULTS' : 
@@ -41,18 +42,24 @@ const ModalSessionResult = ({round, session}) => {
                                 </Text>
                             </View>
                             <View style={styles.lineInfoBar}>
-                                <Text style={styles.lineInfoText}>POS</Text>
-                                <Text style={styles.lineInfoText}>DRIVER</Text>
-                                <Text style={styles.lineInfoText}>TIME/RETIRED</Text>
-                                <Text style={styles.lineInfoText}>FASTEST LAP</Text>
-                                <Text style={styles.lineInfoText}>PTS</Text>
+                                <Text style={[styles.lineInfoText, {width: '10%', textAlign: 'center'}]}>POS</Text>
+                                <Text style={[styles.lineInfoText, {width: '15%', textAlign: 'center'}]}>DRIVER</Text>
+                                <Text style={[styles.lineInfoText, {width: '28%', textAlign: 'center'}]}>TIME/RETIRED</Text>
+                                <Text style={[styles.lineInfoText, {width: '26%', textAlign: 'center'}]}>FASTEST LAP</Text>
+                                <Text style={[styles.lineInfoText, {width: '8%', textAlign: 'center'}]}>PTS</Text>
                             </View>
                             {
                                 results.map((item, index) => {
                                     return (
                                         <View key={index} style={styles.container}>
-                                            <Text>{item?.positionText}</Text>
-                                            <Text>{(item?.Time?.time) ? item?.Time.time : item?.status}</Text>
+                                            <Text style={styles.position}>{item?.positionText}</Text>
+                                            <View style={styles.barCode}>
+                                                <View style={[styles.teamColorBar, {backgroundColor: currentConstructorColor[item?.Constructor?.constructorId]}]}></View>
+                                                <Text style={styles.code}>  {item?.Driver?.code}</Text>
+                                            </View>
+                                            <Text style={styles.time}>{(item?.Time?.time) ? item?.Time.time : item?.status}</Text>
+                                            <Text style={item?.FastestLap?.rank === "1" ? styles.fastestLap1 : styles.fastestLap}>{item?.FastestLap?.Time?.time}</Text>
+                                            <Text style={styles.points}>{item?.points}</Text>
                                         </View>
                                     )
                                 })
@@ -70,7 +77,7 @@ export default ModalSessionResult;
 const styles = StyleSheet.create({
     titleContainer:{        
         width: '100%',
-        padding: 10,
+        padding: 15,
         backgroundColor: '#ff1801',
     },
     title:{
@@ -92,8 +99,59 @@ const styles = StyleSheet.create({
     },
     container:{
         backgroundColor: '#ffffff',
-        flexDirection: 'column',
+        flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'space-around',
+        justifyContent: 'space-between',
+        padding: 5,
+        height: 50,
+        borderColor: 'lightgray',
+        borderWidth: 0.5,
+    },
+    position:{
+        textAlign: 'center',
+        width: '10%',
+        fontWeight: '900',
+    },
+    barCode:{
+        flexDirection: 'row',
+        width: '15%',
+    },
+    teamColorBar:{
+        width: 5,
+        height: 22,
+        borderRadius: 10,
+    },
+    code:{
+        fontWeight: '900',
+        fontSize: 16,
+        textAlignVertical: 'center',
+    },
+    time:{
+        width: '28%',
+        textAlign: 'center',
+        backgroundColor: '#e8e8e8',
+        borderRadius: 20,
+        fontWeight: '600',
+    },
+    fastestLap:{
+        width: '26%',
+        textAlign: 'center',
+        backgroundColor: '#F5F5F5',
+        borderRadius: 20,
+        fontWeight: '500'
+    },
+    fastestLap1:{
+        width: '26%',
+        textAlign: 'center',
+        backgroundColor: '#BF40BF',
+        color: '#ffffff',
+        borderRadius: 20,
+        fontWeight: '500'
+    },
+    points:{
+        width: '8%',
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: '700'
     }
 })
