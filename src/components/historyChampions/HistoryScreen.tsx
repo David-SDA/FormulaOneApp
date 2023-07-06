9da6e65e9e9a7d2f6dace05e47e89e81602db008
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-07-06 17:01:03 +0200

    Debut d'ajout d'une partie listant les circuits

--- src/components/historyChampions/HistoryScreen.tsx
+++ src/components/historyChampions/HistoryScreen.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import HistoryDriversChampions from './historyDriversChampions/HistoryDriversChampionsScreen';
 import HistoryConstructorChampionsScreen from './historyConstructorsChampions/HistoryConstructorChampionsScreen';
+import HistoryCircuits from './historyCircuits/HistoryCircuits';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -24,6 +25,7 @@ const HistoryScreen = () => {
             >
                 <Tab.Screen name='DRIVERS' component={HistoryDriversChampions} />
                 <Tab.Screen name='CONSTRUCTORS' component={HistoryConstructorChampionsScreen} />
+                <Tab.Screen name='TRACKS' component={HistoryCircuits} />
             </Tab.Navigator>
       )
 }
---
+++ src/components/historyChampions/historyCircuits/HistoryCircuits.tsx
@@ -0,0 +1,18 @@
+import { ActivityIndicator, Image, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+
+const HistoryCircuits = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Test</Text>
+        </View>
+    )
+}
+
+export default HistoryCircuits;
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+    }
+})
